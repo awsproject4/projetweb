90a0f98af8574ad5586540098f3c170ba3a0c2de
Anonyme <anon@example.com>
2026-03-11 16:33:27 +0100

    creation des deux tables dans app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./database.db");
 
 const app = express();
 PORT = 3000;
@@ -8,4 +10,31 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
+
+
+
+// Création des tables
+db.serialize(() => {
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT UNIQUE,
+      password TEXT
+    )
+  `);
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      texte TEXT,
+      date_creation DATETIME DEFAULT CURRENT_TIMESTAMP,
+      x INTEGER,
+      y INTEGER,
+      auteur_id INTEGER,
+      FOREIGN KEY (auteur_id) REFERENCES users(id)
+    )
+  `);
+
 });
