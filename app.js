556f334956556184ff3fc1e54621b7e9f5ded084
Anonyme <anon@example.com>
2026-03-12 19:37:39 +0100

    Route /signup : permettant de créer un nouvel utilisateur.

--- app.js
+++ app.js
@@ -4,17 +4,11 @@ const db = new sqlite3.Database("./database.db");
 
 const app = express();
 PORT = 3000;
-app.get("/", (req, res) => {
-    res.send("Serveur fonctionne");
-});
-
-app.listen(PORT, () => {
-    console.log(`Serveur démarré sur http://localhost:${PORT}`);
-});
 
 
 
 // Création des tables
+//La table messages contient une clé étrangère vers users pour relier chaque post-it à son auteur.
 db.serialize(() => {
 
   db.run(`
@@ -37,4 +31,38 @@ db.serialize(() => {
     )
   `);
 
+});
+
+app.get("/", (req, res) => {
+    res.send("Serveur fonctionne");
+});
+
+const path = require("path");
+
+app.get("/signup", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/signup.html"));
+});
+
+const bcrypt = require("bcrypt");
+app.use(express.urlencoded({ extended: true }));
+
+//Les mots de passe sont hachés avec bcrypt avant stockage pour éviter qu’ils soient enregistrés en clair.
+app.post("/signup", async (req, res) => {
+
+  const { username, password } = req.body;
+
+  const hash = await bcrypt.hash(password, 10);
+
+  db.run(
+    "INSERT INTO users (username, password) VALUES (?, ?)",
+    [username, hash],
+    (err) => {
+      if (err) return res.send("Utilisateur déjà existant");
+      res.redirect("/");
+    }
+  );
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
